bcd90cd5633465858311d80ccd13332a9192e6b4
minimalistPaulius <remeikucis1@gmail.com>
2020-07-18 02:42:33 +0200

    finished 3rd task

--- src/com/company/Lektuvas.java
+++ src/com/company/Lektuvas.java
@@ -8,10 +8,18 @@ public class Lektuvas {
         Random rand = new Random();
         int atsitiktinisSkaicius = rand.nextInt(10);
         try {
-            System.out.println(4 / atsitiktinisSkaicius);
+            int x = 5 / atsitiktinisSkaicius;
             System.out.println("OK: važiuoklė sėkmingai išskleista");
+            ///////////////////////////////////////////////////////////
+            if (x == 5) {
+                ("skrendu").charAt(20);
+            }
         } catch (ArithmeticException e) {
             System.out.println("ERROR: važiuoklės išskleisti nepavyko");
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("ERROR: nepavyko išskleisti važiuoklės dėl kitos klaidos");
+        } finally {
+            System.out.println("INFO: lėktuvas skrenda");
         }
     }
 
